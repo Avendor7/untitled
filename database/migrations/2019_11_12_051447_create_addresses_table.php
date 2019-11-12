8e500384fcc75a4ad63cb4995de0cfe5fbb5287c
Stephen <steve.wiggins7@gmail.com>
2019-11-12 00:21:00 -0500

    migration code

--- database/migrations/2019_11_12_051447_create_addresses_table.php
+++ database/migrations/2019_11_12_051447_create_addresses_table.php
@@ -16,6 +16,12 @@ class CreateAddressesTable extends Migration
         Schema::create('addresses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->varchar('line1');
+            $table->varchar('line2');
+            $table->varchar('postalcode');
+            $table->varchar('phone');
+            $table->varchar('city');
+            $table->varchar('province');
         });
     }
 
