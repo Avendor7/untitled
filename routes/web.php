d56c3e69ea97c064935912201006f9262e851979
Stephen <steve.wiggins7@gmail.com>
2019-11-16 00:48:22 -0500

    device meta

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('networks','NetworkController');
 Route::resource('devices','DevicesController');
+Route::resource('devicemeta','DeviceMetaController');
