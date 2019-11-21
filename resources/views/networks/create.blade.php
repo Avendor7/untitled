4b56a5cf13b11b171e10a3af1a61280440b0eac7
Stephen <steve.wiggins7@gmail.com>
2019-11-20 22:46:41 -0500

    move folders

--- resources/views/networks/create.blade.php
+++ resources/views/networks/create.blade.php
@@ -7,7 +7,7 @@
             <h2>Add New Product</h2>
         </div>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            <a class="btn btn-primary" href="{{ route('networks.index') }}"> Back</a>
         </div>
     </div>
 </div>
@@ -23,7 +23,7 @@
     </div>
 @endif
    
-<form action="{{ route('products.store') }}" method="POST">
+<form action="{{ route('networks.store') }}" method="POST">
     @csrf
   
      <div class="row">
