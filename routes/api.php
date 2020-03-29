725faf287165368a833dd4dd0b2d50717f4bce41
Stephen <steve.wiggins7@gmail.com>
2020-03-29 00:26:39 -0400

    add some API stuff instead of Web stuff

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/network', 'NetworkController@index');
+Route::get('/network/{id}', 'NetworkController@show');
+Route::post('/network', 'NetworkController@store');
+Route::post('/network/{id}', 'NetworkController@update');
+Route::delete('/network/{id}', 'NetworkController@delete');
